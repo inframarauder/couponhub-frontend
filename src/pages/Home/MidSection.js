829ed570a8d884c07f5c1e8a6455d7bf3f804cc5
subhasis020299 <subhasisdas125@gmail.com>
2021-03-19 15:20:08 +0530

    hide signup/login when user is already loggedin

--- src/pages/Home/MidSection.js
+++ src/pages/Home/MidSection.js
@@ -1,28 +1,37 @@
 import React from "react";
 import { Jumbotron, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const MidSection = () => {
+const MidSection = ({ auth }) => {
   return (
     <Jumbotron fluid>
       <Container className="center-content">
         <p className="display-3">CouponHub</p>
         <p>Ek haath se dene ka, doosre haath se lene ka....</p>
-        <p>
-          <Button
-            variant="outline-success"
-            size="lg"
-            onClick={() => (window.location.href = "/signup")}
-          >
-            Sign Up
-          </Button>
-        </p>
-        <p>
-          Already have an account? &nbsp;<Link to="/login">Log In</Link>
-        </p>
+        {!auth.isLoggedIn && (
+          <>
+            <p>
+              <Button
+                variant="outline-success"
+                size="lg"
+                onClick={() => (window.location.href = "/signup")}
+              >
+                Sign Up
+              </Button>
+            </p>
+            <p>
+              Already have an account? &nbsp;<Link to="/login">Log In</Link>
+            </p>
+          </>
+        )}
       </Container>
     </Jumbotron>
   );
 };
 
-export default MidSection;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, null)(MidSection);
