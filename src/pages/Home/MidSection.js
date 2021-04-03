5d6cafc9cdafe381b575180ee4cd5a3a24639fa4
Shuvam Manna <shuvam.manna@gmail.com>
2021-04-03 14:28:05 +0530

    Update MidSection.js

--- src/pages/Home/MidSection.js
+++ src/pages/Home/MidSection.js
@@ -30,7 +30,7 @@ const MidSection = ({ auth }) => {
               </span>
             </p>
             <p style={{ color: "white", fontWeight: "700", fontSize: "20px" }}>
-              &nbsp;&nbsp;What do you do with extra coupons that you would not need? What if you could swap them with something that you would use?<br/>
+              What do you do with extra coupons that you would not need? What if you could swap them with something that you would use?<br/>
 Sounds cool? You are at the right place ;)
             </p>
             {!auth.isLoggedIn && (
