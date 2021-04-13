e0006e1af3ecae2ef00b5e28dba576f320d4511e
subhasis020299 <subhasisdas125@gmail.com>
2021-04-13 21:32:07 +0530

    added tab view for coupons list

--- src/pages/Coupons/index.js
+++ src/pages/Coupons/index.js
@@ -1,14 +1,33 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Tabs, Tab } from "react-bootstrap";
 import FilterForm from "./FilterForm";
 import CouponList from "./CouponList";
 
 const Coupons = () => {
+  const [key, setKey] = useState("All");
+
   return (
     <Container className="my-4 h-100">
       <FilterForm />
       <legend className="searchBoxText">🤗 available coupons</legend>
-      <CouponList />
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="All" title="All">
+          <CouponList type={key} />
+        </Tab>
+        <Tab eventKey="percentage" title="Percentage">
+          <CouponList type={key} />
+        </Tab>
+        <Tab eventKey="flat" title="Flat">
+          <CouponList type={key} />
+        </Tab>
+        <Tab eventKey="free" title="Free">
+          <CouponList type={key} />
+        </Tab>
+      </Tabs>
     </Container>
   );
 };
